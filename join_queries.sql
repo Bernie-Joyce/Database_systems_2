80cd6492292f679b689796bfdb867cd07305abcc
MichalSalabura <D00282723@student.dkit.ie>
2025-10-16 14:08:35 +0100

    Update join_queries.sql

--- join_queries.sql
+++ join_queries.sql
@@ -1,1 +1,6 @@
-
+-- Select all pokemon, and their types
+SELECT p.name, t.type_name
+FROM pokemon AS p
+LEFT JOIN pokemontypes AS pt ON p.pokemon_id = pt.pokemon_id
+LEFT JOIN types AS t ON t.type_id = pt.type_id
+WHERE t.type_name = 'Normal';
