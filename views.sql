9589eb1f49bad28ea790f1120cd1bdeaabe8a310
MichalSalabura <D00282723@student.dkit.ie>
2025-10-17 00:10:45 +0100

    Add: Stats view

--- views.sql
+++ views.sql
@@ -1,1 +1,20 @@
-
+-- View that calculates stats for each caught pokemon based on their base stats and IVs
+CREATE VIEW TrainerPokemonStats AS
+SELECT
+    tp.caught_id,
+    tp.trainer_id,
+    tp.nick_name AS Name,
+    p.name AS Species,
+    tp.pokemon_level as Level,
+    
+    -- Calculated HP
+    FLOOR(((p.base_hp + tp.hit_points_iv) * 2 * tp.pokemon_level) / 100) + tp.pokemon_level + 10 AS HP,
+    
+    -- Calculated Attack
+    FLOOR(((p.base_attack + tp.attack_iv) * 2 * tp.pokemon_level) / 100) + 5 AS Attack,
+    
+    -- Calculated Defense
+    FLOOR(((p.base_defense + tp.defense_iv) * 2 * tp.pokemon_level) / 100) + 5 AS Defense
+    
+FROM trainerpokemon tp
+JOIN pokemon p ON tp.pokemon_id = p.pokemon_id;
