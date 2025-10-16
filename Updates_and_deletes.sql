c6863dae2aeae5b1257b04dda232c88d8320835f
MichalSalabura <D00282723@student.dkit.ie>
2025-10-17 00:37:13 +0100

    Create Updates_and_deletes.sql

---
+++ Updates_and_deletes.sql
@@ -0,0 +1,8 @@
+-- Update pokemon level by 1 for each pokemon with trainer_id = 4
+UPDATE trainerpokemon AS tp
+SET tp.pokemon_level = tp.pokemon_level + 1
+WHERE tp.trainer_id = 4;
+
+-- Delete all of trainer 6's pokemon
+DELETE FROM trainerpokemon
+WHERE trainer_id = 6
