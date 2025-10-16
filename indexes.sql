bc21096ecabcc8d280e34a3af4b26738574b5d5d
MichalSalabura <D00282723@student.dkit.ie>
2025-10-17 00:21:26 +0100

    Add: Indexes between wildpokemon, pokemon and region
    These speed up looking for pokemon, their region, location and level range

--- indexes.sql
+++ indexes.sql
@@ -1,1 +1,15 @@
-
+-- Index for the region join
+CREATE INDEX index_wildpokemon_region ON wildpokemon(region_id);
+-- Index for the pokemon join
+CREATE INDEX index_wildpokemon_pokemon ON wildpokemon(pokemon_id);
+-- To show indexes being used run
+EXPLAIN
+SELECT 
+    p.name AS pokemon_name,
+    r.name AS region,
+    w.location_description,
+    w.min_level,
+    w.max_level
+FROM wildpokemon AS w
+JOIN region AS r ON w.region_id = r.region_id
+JOIN pokemon AS p ON w.pokemon_id = p.pokemon_id;
