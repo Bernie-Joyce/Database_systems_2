b6b9bda519bf6050e2a0583fe8608fb8348f2ef1
smiichyk <https://github.com/smiichyk>
2025-10-18 19:11:00 +0100

    Changed table name from TYPE Types to follow naming convention

--- join_queries.sql
+++ join_queries.sql
@@ -1,6 +1,7 @@
 -- Select all pokemon, and their types
 SELECT p.name, t.type_name
-FROM pokemon AS p
-LEFT JOIN pokemontypes AS pt ON p.pokemon_id = pt.pokemon_id
-LEFT JOIN types AS t ON t.type_id = pt.type_id
+FROM Pokemon AS p
+LEFT JOIN PokemonTypes AS pt ON p.pokemon_id = pt.pokemon_id
+LEFT JOIN Types AS t ON t.type_id = pt.type_id
 WHERE t.type_name = 'Normal';
+
