1e64df29ff52a3f7a1fda5d7c65694c2be9c9ac9
smiichyk <https://github.com/smiichyk>
2025-10-18 19:06:16 +0100

    Changed table name from TYPE Types to follow naming convention

--- indexes.sql
+++ indexes.sql
@@ -1,7 +1,9 @@
 -- Index for the region join
-CREATE INDEX index_wildpokemon_region ON wildpokemon(region_id);
+CREATE INDEX index_wildpokemon_region ON WildPokemon(region_id);
+
 -- Index for the pokemon join
-CREATE INDEX index_wildpokemon_pokemon ON wildpokemon(pokemon_id);
+CREATE INDEX index_wildpokemon_pokemon ON WildPokemon(pokemon_id);
+
 -- To show indexes being used run
 EXPLAIN
 SELECT 
@@ -10,6 +12,6 @@ SELECT
     w.location_description,
     w.min_level,
     w.max_level
-FROM wildpokemon AS w
-JOIN region AS r ON w.region_id = r.region_id
-JOIN pokemon AS p ON w.pokemon_id = p.pokemon_id;
+FROM WildPokemon AS w
+JOIN Region AS r ON w.region_id = r.region_id
+JOIN Pokemon AS p ON w.pokemon_id = p.pokemon_id;
