78c27c4cc6a17abe712c5da4982c834b7714e5e7
Nikita Smiichyk <157646619+smiichyk@users.noreply.github.com>
2025-10-19 12:33:16 +0100

    add user and pass

--- DBConnectJava/DBConnect.java
+++ DBConnectJava/DBConnect.java
@@ -6,6 +6,8 @@ public class DBConnect {
 
     // Path to the SQLite database file
     private static final String DB_URL = "jdbc:mysql://localhost:3306/Pokemon";
+    private static final String USER = "root";
+    private static final String PASS = "smiichyk";
 
     /**
      * Establishes and returns a connection to the SQLite database.
@@ -15,7 +17,7 @@ public class DBConnect {
     public static Connection connect() {
         try {
             // Attempt to establish a connection to the database
-            return DriverManager.getConnection(DB_URL);
+            return DriverManager.getConnection(DB_URL, USER, PASS);
         } catch (SQLException e) {
             // Print error message if connection fails
             System.out.println("Connection failed: " + e.getMessage());
